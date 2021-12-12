34080082cff467bbc5cb7ceb4d258a1829ee4a89
rcgc <a01702388@itesm.mx>
2021-12-11 19:11:22 -0600

    1512A function name refactored

--- 1512A.cpp
+++ 1512A.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 
-int find_spy(int n, int arr[100]){
+int find_different(int n, int arr[100]){
     if(arr[0]!=arr[1] && arr[0]!=arr[2]) return 1;
     if(arr[1]!=arr[0] && arr[1]!=arr[2]) return 2;
     if(arr[2]!=arr[0] && arr[2]!=arr[1]) return 3;
@@ -16,6 +16,6 @@ int main(){
     for(int i=0; i<t; i++){
         cin >> n;
         for(int j=0; j<n; j++){ cin >> arr[j]; }
-        cout << find_spy(n, arr) << endl;
+        cout << find_different(n, arr) << endl;
     } return 0;
 }
