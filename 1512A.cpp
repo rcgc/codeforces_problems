4d053a6e1cd99ab20aa28351a2d45437906324a7
rcgc <a01702388@itesm.mx>
2021-12-11 19:10:09 -0600

    1512A finished

---
+++ 1512A.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int find_spy(int n, int arr[100]){
+    if(arr[0]!=arr[1] && arr[0]!=arr[2]) return 1;
+    if(arr[1]!=arr[0] && arr[1]!=arr[2]) return 2;
+    if(arr[2]!=arr[0] && arr[2]!=arr[1]) return 3;
+
+    for(int i=3; i<n; i++){ if(arr[i] != arr[i-1]) return i+1; }
+    return -1;
+}
+
+int main(){
+    int t, n, arr[100];
+    cin >> t;
+    for(int i=0; i<t; i++){
+        cin >> n;
+        for(int j=0; j<n; j++){ cin >> arr[j]; }
+        cout << find_spy(n, arr) << endl;
+    } return 0;
+}
