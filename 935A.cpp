b2f06a0e7d2beb8a2498045f1e05c4cc8328cdd1
rcgc <a01702388@itesm.mx>
2022-01-10 23:15:41 -0600

    935A solved

---
+++ 935A.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n, counter=0;
+    cin >> n;
+    for(int i=1; i<=n/2; i++) if(n%i==0) counter++;
+    cout << counter << endl;
+    return 0;
+}
