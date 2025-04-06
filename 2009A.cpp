3cffa66652874e3f4127185c933e3b15457d2423
rcgc <roberto140298@gmail.com>
2025-04-06 16:59:17 -0600

    2009A solved

---
+++ 2009A.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int t, a, b;
+    cin >> t;
+    for(int i=0; i<t; i++){
+        cin >> a >> b;
+        cout << b-a << endl;
+    }
+    return 0;
+}
